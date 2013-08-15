12bd7fbaf4301e44e7be5cc05334c46907678cd5
Josh Bielick <josh@fragmentlabs.com>
2013-08-15 17:57:32 -0400

    fixed concurrent ajax requests bugginess

--- HistoryController.js
+++ HistoryController.js
@@ -6,6 +6,7 @@ function HistController() {
 	var _this = this
 	this.url = window.location.pathname
 	this.cache = {}
+	this.requests = 0
 	this.view = '#view'
 	this.start = false
 	this.init = function() {
@@ -19,6 +20,7 @@ function HistController() {
 		if(_this.url in _this.cache) {
 			_this.render(_this.cache[_this.url], false)
 		} else {
+			_this.requests++
 			$.ajax({
 				url: _this.url,
 				type: 'GET',
@@ -30,6 +32,9 @@ function HistController() {
 					_this.render(xhr.responseText, true)
 					if(!(/^4/.test(xhr.status)))
 						$('#view').prepend($('<div id="flashMessage">An Error Occurred: ('+msg+')<br>'+xhr.responseText+'</div>'))
+				},
+				complete: function() {
+					_this.requests--
 				}
 			})
 		}
@@ -46,25 +51,27 @@ function HistController() {
 		window.history.replaceState(JSON.stringify(data), document.title, data.url)
 	},
 	this.render = function(data, push) {
-		var push = (typeof push === 'undefined') ? true : push
-		try {
-			data = JSON.parse(data)
-		} catch(e) {
-			data = {html:data, url: _this.url}
-		}
-		_this.saveState()
-		if(window.history.pushState) {
-			if(push) {
-				_this._cache(data.url, data.html)
-				window.history.pushState(JSON.stringify(data), null, data.url)
-			} else {
-				window.history.replaceState(JSON.stringify(data), null, data.url)
+		if(_this.requests < 2) {
+			var push = (typeof push === 'undefined') ? true : push
+			try {
+				data = JSON.parse(data)
+			} catch(e) {
+				data = {html:data, url: _this.url}
 			}
-		} else if (push) {
-			// window.location.hash = '!'+data.url.substring(data.url.indexOf('#'), -1)
+			_this.saveState()
+			if(window.history.pushState) {
+				if(push) {
+					_this._cache(data.url, data.html)
+					window.history.pushState(JSON.stringify(data), null, data.url)
+				} else {
+					window.history.replaceState(JSON.stringify(data), null, data.url)
+				}
+			} else if (push) {
+				// window.location.hash = '!'+data.url.substring(data.url.indexOf('#'), -1)
+			}
+			$(_this.view).html(data.html)
+			_this.init()
 		}
-		$(_this.view).html(data.html)
-		_this.init()
 	},
 	this._cache = function(url, data) {
 		if(!(url in _this.cache)) {
